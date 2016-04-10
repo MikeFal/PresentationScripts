ba9c096c005999cce1ee633929b9ca3c4f853979
Mike F <mike@mikefal.net>
2016-04-10 08:44:45 -0600

    Updates from SQLSatMadison

---
+++ ExploringSQLPS/ExploringSQLPS.ps1
@@ -0,0 +1,126 @@
+﻿#Where does the module live?
+Get-Module -ListAvailable *SQL*
+
+#Lets look in that location and check out some of the files.
+dir 'C:\Program Files (x86)\Microsoft SQL Server\120\Tools\PowerShell\Modules\SQLPS'
+
+powershell_ise 'C:\Program Files (x86)\Microsoft SQL Server\120\Tools\PowerShell\Modules\SQLPS\SQLPS.PS1'
+powershell_ise 'C:\Program Files (x86)\Microsoft SQL Server\120\Tools\PowerShell\Modules\SQLPS\SqlPsPostScript.PS1'
+
+
+#Cool, now load the module
+Import-Module SQLPS
+
+#What's that warning?
+Import-Module SQLPS -Verbose
+Import-Module SQLPS -DisableNameChecking
+
+#Ok, so what's actually in it?
+Get-Command -Module SQLPS
+Get-Command -Module SQLPS | Measure-Object
+
+#-----------------------------------------
+#Using the provider
+Get-PSDrive
+Get-PSDrive -PSProvider SqlServer
+
+cd SQLSERVER:\
+dir
+
+CD SQL
+dir
+#Only the local machine is visible 
+
+CD PICARD
+dir
+
+CD DEFAULT
+dir
+
+CD databases
+dir
+
+#Notice what type of objects these are
+Get-Item AdventureWorks2014 | Get-Member
+
+#We can make use of these objects
+dir | select name,createdate,@{name='DataSizeMB';expression={$_.dataspaceusage/1024}} | Format-Table -AutoSize
+
+#We can drill further down
+
+dir AdventureWorks2014\Tables
+dir AdventureWorks2014\StoredProcedures
+
+#How is this different than system views? Going across multiple servers
+$servers = @('PICARD','RIKER')
+$servers | ForEach-Object {dir SQLSERVER:\SQL\$_\DEFAULT\DATABASES} | select @{n='Server';e={$_.Parent.Name}},name,createdate,@{name='DataSizeMB';expression={$_.dataspaceusage/1024}} | Format-Table -AutoSize
+
+#-----------------------------------------
+#Using the cmdlets
+
+Get-Command -Module SQLPS
+Get-Command -Module SQLPS | Measure-Object
+
+Get-SqlDatabase -ServerInstance PICARD -Name AdventureWorks2014
+
+Get-SqlInstance -MachineName PICARD
+
+Backup-SqlDatabase -ServerInstance PICARD -Database AdventureWorks2014  -BackupFile 'C:\TEMP\AdventureWorks2014.bak' -Initialize -CopyOnly -Script
+
+Backup-SqlDatabase -ServerInstance PICARD -Database AdventureWorks2014  -BackupFile 'C:\TEMP\AdventureWorks2014.bak' -Initialize -CopyOnly
+
+$sql=@'
+SET NOCOUNT ON
+select sp.name,count(1) db_count
+from sys.server_principals sp
+join sys.databases d on (sp.sid = d.owner_sid)
+group by sp.name
+'@
+
+$sqlcmdout = sqlcmd -S PICARD -d tempdb -Q $sql
+$invokesqlout = Invoke-Sqlcmd -ServerInstance PICARD -Database tempdb -Query $sql
+
+
+$sqlcmdout
+$invokesqlout
+
+$sqlcmdout[0].GetType()
+$invokesqlout[0].GetType()
+
+#Practical use
+$instances = @(’PICARD’,’RIKER’)
+
+#Check your SQL Server versions
+$instances | ForEach-Object {Get-Item “SQLSERVER:\SQL\$_\DEFAULT”} | Select-Object Name,VersionString
+
+#Check your databases for last backup
+$instances | ForEach-Object {Get-ChildItem “SQLSERVER:\SQL\$_\DEFAULT\Databases”} |
+    Sort-Object Size -Descending | 
+    Select-Object @{n='Server';e={$_.parent.Name}},Name,LastBackupDate,Size
+
+#Backup up your system databases
+foreach($instance in $instances){
+    $dbs = Get-ChildItem SQLSERVER:\SQL\$instance\DEFAULT\Databases -Force |Where-Object {$_.IsSystemObject -eq $true -and $_.Name -ne 'TempDB'}
+    $dbs |ForEach-Object {Backup-SqlDatabase -ServerInstance $instance -Database $_.Name -BackupFile "C:\Backups\$($_.Name).bak" -Initialize }
+    }
+$instances |ForEach-Object {Invoke-Command -ComputerName $_ -ScriptBlock {Get-ChildItem C:\Backups\*.bak}}
+
+#Creating a point in time restore script
+Set-Location C:\Temp
+$LastFull= Get-ChildItem '\\PICARD\C$\Backups\AdventureWorks2014\*.bak' | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+$logs = Get-ChildItem '\\PICARD\C$\Backups\AdventureWorks2014\*.trn' | Where-Object {$_.LastWriteTime -gt $LastFull.LastWriteTime} | Sort-Object LastWriteTime
+
+$MoveFiles = @()
+$MoveFiles += New-Object Microsoft.SqlServer.Management.Smo.RelocateFile ('AdventureWorks2014_Data','C:\DBFiles\data\AdventureWorks2014New_Data.mdf')
+$MoveFiles += New-Object Microsoft.SqlServer.Management.Smo.RelocateFile ('AdventureWorks2014_Log','C:\DBFiles\log\AdventureWorks2014New_Log.ldf')
+
+$db = 'AdventureWork2014New'
+Restore-SqlDatabase -ServerInstance 'PICARD' -Database $db -RelocateFile $MoveFiles -BackupFile $LastFull.FullName -RestoreAction Database -NoRecovery -Script | Out-File 'C:\Temp\Restore.sql'
+foreach($log in $logs){
+    if($log -eq $logs[$logs.Length -1]){
+        Restore-SqlDatabase -ServerInstance 'PICARD' -Database $db -BackupFile $log.FullName -RestoreAction Log -Script | Out-File 'C:\Temp\Restore.sql' -Append
+    }
+    else{
+        Restore-SqlDatabase -ServerInstance 'PICARD' -Database $db -BackupFile $log.FullName -RestoreAction Log -NoRecovery -Script | Out-File 'C:\Temp\Restore.sql' -Append
+    }
+}
---
+++ ManagingAGs/ManagingAGGroups.ps1
@@ -0,0 +1,99 @@
+﻿#Login Demo
+
+#Clean Old stuff
+$dropquery = @'
+IF EXISTS (SELECT 1 FROM sys.server_principals WHERE name = 'adw_readonly') DROP LOGIN adw_readonly;
+USE AdventureWorks2014;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'adw_readonly') DROP USER adw_readonly;
+'@
+Invoke-SqlCmd -ServerInstance PICARD -Database tempdb -Query $dropquery
+Invoke-SqlCmd -ServerInstance RIKER -Database tempdb -Query $dropquery
+
+$query1 = @"
+IF EXISTS (SELECT 1 FROM sys.server_principals WHERE name = 'adw_readonly') DROP LOGIN adw_readonly;
+CREATE LOGIN adw_readonly WITH PASSWORD='password',CHECK_POLICY=OFF;
+USE AdventureWorks2014;
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'adw_readonly') DROP USER adw_readonly;
+CREATE USER adw_readonly FROM LOGIN adw_readonly;
+GRANT SELECT ON database::[AdventureWorks2014] to adw_readonly;
+"@
+
+Invoke-SqlCmd -ServerInstance ENTERPRISE -Database tempdb -Query $query1
+
+Invoke-Sqlcmd -ServerInstance ENTERPRISE -Database AdventureWorks2014 -Username adw_readonly -Password 'password' -Query 'select top(10) FirstName,LastName,ModifiedDate from person.person;'
+
+Switch-SqlAvailabilityGroup -Path SQLSERVER:\SQL\RIKER\DEFAULT\AvailabilityGroups\ENTERPRISE
+
+Invoke-Sqlcmd -ServerInstance ENTERPRISE -Database AdventureWorks2014 -Username adw_readonly -Password 'password' -Query 'select top(10) FirstName,LastName,ModifiedDate from person.person;'
+
+$query2 = @"
+IF EXISTS (SELECT 1 FROM sys.server_principals WHERE name = 'adw_readonly') DROP LOGIN adw_readonly;
+CREATE LOGIN adw_readonly WITH PASSWORD='password',CHECK_POLICY=OFF;
+"@
+Invoke-SqlCmd -ServerInstance ENTERPRISE -Database tempdb -Query $query2
+
+Invoke-Sqlcmd -ServerInstance ENTERPRISE -Database AdventureWorks2014 -Username adw_readonly -Password 'password' -Query 'select top(10) FirstName,LastName,ModifiedDate from person.person;'
+
+. 'C:\Program Files\WindowsPowerShell\Copy-SQLLogins.ps1'
+
+Copy-SQLLogins -source PICARD -logins 'adw_readonly' 
+Copy-SQLLogins -source PICARD -logins 'adw_readonly' -ApplyTo RIKER
+
+Invoke-Sqlcmd -ServerInstance ENTERPRISE -Database AdventureWorks2014 -Username adw_readonly -Password 'password' -Query 'select top(10) FirstName,LastName,ModifiedDate from person.person;'
+
+#Failback to PICARD
+Switch-SqlAvailabilityGroup -Path SQLSERVER:\SQL\PICARD\DEFAULT\AvailabilityGroups\ENTERPRISE -
+
+
+#Backup Demo
+#cleanup
+$dropjobquery = "USE msdb;EXEC sp_delete_job @job_name='Backup AdventureWorks - FULL';"
+Invoke-SqlCmd -ServerInstance PICARD -Database tempdb -Query $dropjobquery
+Invoke-SqlCmd -ServerInstance RIKER -Database tempdb -Query $dropjobquery
+
+#Let's create a job
+$smosrv = New-Object Microsoft.SqlServer.Management.Smo.Server 'PICARD'
+$job = New-Object Microsoft.SqlServer.Management.Smo.Agent.Job ($smosrv.JobServer,'Backup AdventureWorks - FULL')
+$job.Description = 'Demo Backup Job for AdventureWorks2014'
+
+$jobstep = new-object Microsoft.SqlServer.Management.Smo.Agent.JobStep ($job, 'Execute Script')
+$jobstep.SubSystem = 'TransactSQL'
+$jobstep.Command = "BACKUP DATABASE AdventureWorks2014 TO DISK = 'C:\Backups\ADW2014_AGs.bak' WITH INIT;"
+$jobstep.OnSuccessAction = 'QuitWithSuccess'
+$jobstep.OnFailAction = 'QuitWithFailure'
+$job.Create()
+$jobstep.Create()
+$job.ApplyToTargetServer($smosrv.Name)
+
+$job.Start()
+
+#Run SQL query for backup info - PICARD
+
+#Failover
+Switch-SqlAvailabilityGroup -Path SQLSERVER:\SQL\RIKER\DEFAULT\AvailabilityGroups\ENTERPRISE
+
+#Let's create a job on RIKER
+$smosrv = New-Object Microsoft.SqlServer.Management.Smo.Server 'RIKER'
+$job = New-Object Microsoft.SqlServer.Management.Smo.Agent.Job ($smosrv.JobServer,'Backup AdventureWorks - FULL')
+$job.Description = 'Demo Backup Job for AdventureWorks2014'
+
+$jobstep = new-object Microsoft.SqlServer.Management.Smo.Agent.JobStep ($job, 'Execute Script')
+$jobstep.SubSystem = 'TransactSQL'
+$jobstep.Command = "BACKUP DATABASE AdventureWorks2014 TO DISK = 'C:\Backups\ADW2014_AGs.bak' WITH INIT;"
+$jobstep.OnSuccessAction = 'QuitWithSuccess'
+$jobstep.OnFailAction = 'QuitWithFailure'
+$job.Create()
+$jobstep.Create()
+$job.ApplyToTargetServer($smosrv.Name)
+
+$job.Start()
+
+#Run SQL query for backup info - RIKER
+
+
+#PATCHING info
+Get-Item SQLSERVER:\SQL\PICARD\DEFAULT | select Name,Version
+Get-Item SQLSERVER:\SQL\RIKER\DEFAULT | select Name,Version
+
+Get-Item SQLSERVER:\SQL\PICARD\DEFAULT\DATABASES\AdventureWorks2014 | select Name,Version
+Get-Item SQLSERVER:\SQL\RIKER\DEFAULT\DATABASES\AdventureWorks2014 | select Name,Version
